35adde5d7dda871cce23000fdc80be1346bcae15
Brian Fountain <brian@brianfountain.com>
2018-07-31 08:06:31 -0400

    Update day 9 and 10 materials

--- Lessons/Day_08/stoplight/js/jquery_stoplight.js
+++ Lessons/Day_08/stoplight/js/jquery_stoplight.js
@@ -5,5 +5,33 @@ function stop() {
   $('#slowLight').css('background', '#111');
   $('#goLight').css('background', '#111');
 }
+
+function slow() {
+  // set red bulb to red
+  $('#stopLight').css('background', '#111');
+  $('#slowLight').css('background', 'yellow');
+  $('#goLight').css('background', '#111');
+}
+
+function go() {
+  // set red bulb to red
+  $('#stopLight').css('background', '#111');
+  $('#slowLight').css('background', '#111');
+  $('#goLight').css('background', 'green');
+}
+
+function next() {
+  if (document.getElementById('stopLight').style.background === "red") {
+    go();
+  } else if (document.getElementById('slowLight').style.background === "yellow") {
+    stop();
+  } else {
+    slow();
+  }
+}
+
 // attached click handler to button
 $('#stopButton').click(stop);
+$('#slowButton').click(slow);
+$('#go').click(go);
+$('#header').click(next);
